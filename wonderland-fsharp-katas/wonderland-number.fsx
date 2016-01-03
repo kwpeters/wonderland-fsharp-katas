58bd85189e66eaf36a09bb5faa5ded384c4284eb
epeicher <ese.rober@gmail.com>
2016-01-03 11:56:39 +0100

    wonderland-number solved

--- wonderland-fsharp-katas/wonderland-number.fsx
+++ wonderland-fsharp-katas/wonderland-number.fsx
@@ -1,9 +1,23 @@
 // See the file wonderland-number.md for detailed information.
 
-let wonderlandNumber () = 42
-
 let haveSameDigits (n1:int,n2:int) =
     (string n1 |> Set.ofSeq) = (string n2 |> Set.ofSeq)
+
+let multiplicands = [2..6]
+let sixDigits = [1000000..10000000/List.last multiplicands]
+
+let isWonderlandNumber x =
+    List.fold (fun acc n -> acc && haveSameDigits (x,n)) true multiplicands
+
+let wonderlandNumber () = 
+    let rec findNum i =
+        if i>List.last sixDigits then -1    // Safety check
+        else if isWonderlandNumber i then i
+        else
+            findNum (i + 1)
+
+    findNum (List.head sixDigits)
+
 
 #r @"../packages/Unquote/lib/net45/Unquote.dll"
 open Swensen.Unquote
