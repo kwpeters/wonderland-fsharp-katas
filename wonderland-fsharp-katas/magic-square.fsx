96b1205e5cf4cd683e0e7ff9e02a4ade85fd3831
epeicher <ese.rober@gmail.com>
2016-01-08 00:37:04 +0100

    magic-square solved

--- wonderland-fsharp-katas/magic-square.fsx
+++ wonderland-fsharp-katas/magic-square.fsx
@@ -2,20 +2,9 @@
 
 let values = [| 1.0 .. 0.5 .. 5.0 |]
 
-type Square = float[,]
-
 let dim = 3
 
-let magicSquare () =
-    // fix this so that the tests pass!
-    let square = Array2D.init dim dim (fun row col ->
-        values.[row * dim + col])
-    square
-
-
-#r @"../packages/Unquote/lib/net45/Unquote.dll"
-open Swensen.Unquote
-
+type Square = float[,]
 let maxIndex = dim - 1
 let indexes = [ 0 .. maxIndex ]
 
@@ -34,6 +23,40 @@ let sumDownDiagonal (sq:Square) =
 let sumUpDiagonal (sq:Square) =
     [ for i in indexes -> sq.[i, maxIndex - i] ] |> List.sum
 
+let testSumRows magic = 
+    indexes |> List.map (sumRow magic) |> Set.ofList |> Set.count = 1
+
+let testSumCols magic = 
+    indexes |> List.map (sumColumn magic) |> Set.ofList |> Set.count = 1
+
+let testSumDiagonals magic =
+    sumDownDiagonal magic = sumUpDiagonal magic
+
+let getPermutations array =
+    let rec permutations list taken = 
+      seq { if Set.count taken = List.length list then yield [] else
+            for l in list do
+              if not (Set.contains l taken) then 
+                for perm in permutations list (Set.add l taken)  do
+                  yield l::perm }
+    permutations (Array.toList array) Set.empty
+
+
+let isMagicSquare square =
+    (testSumRows square) && (testSumCols square) && (testSumDiagonals square)
+   
+let magicSquare () =
+    getPermutations values
+    |> Seq.map (fun v -> 
+        Array2D.init dim dim (fun row col -> 
+            v.[row * dim + col]))
+    |> Seq.filter isMagicSquare
+    |> Seq.head
+
+
+
+#r @"../packages/Unquote/lib/net45/Unquote.dll"
+open Swensen.Unquote
 
 let tests () =
 
