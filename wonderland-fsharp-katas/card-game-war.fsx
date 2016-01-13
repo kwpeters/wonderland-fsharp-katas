f88de219850bb49ae10b50e489b34ab8184232fb
epeicher <ese.rober@gmail.com>
2016-01-14 00:29:55 +0100

    First solution

--- wonderland-fsharp-katas/card-game-war.fsx
+++ wonderland-fsharp-katas/card-game-war.fsx
@@ -17,15 +17,35 @@ type Rank =
 
 type Card = Suit * Rank
 
-let playRound (card1:Card,card2:Card) =
-    failwith "not implemented: winning card"
-
-let playGame (hand1:Card list, hand2:Card list) =
-    failwith "not implemented: game winner"
-
-(*
 let suits = [ Spade; Club; Diamond; Heart ]
 let heads = [ Jack; Queen; King; Ace ]
+
+let playRound (card1:Card,card2:Card) =
+    let greaterIndexFirst e1 e2 l =
+        List.findIndex (fun r -> r = e1) l > List.findIndex (fun r -> r = e2) l
+        
+    match card1,card2 with
+    | (s1,Value x), (s2,Value y) -> 
+        if x = y then greaterIndexFirst s1 s2 suits
+        else x > y
+    | (s1,Value x), (s2,_) -> false
+    | (s1,_),(s2,Value y) -> true
+    | (s1,r1),(s2,r2) -> 
+        if r1 = r2 then 
+            greaterIndexFirst s1 s2 suits
+        else greaterIndexFirst r1 r2 heads 
+        
+
+let rec playGame (hand1:Card list, hand2:Card list) =
+    match hand1,hand2 with
+    | [],_ -> printfn "Player 2 wins"
+    | _,[] -> printfn "Player 1 wins"
+    | c1::xs,c2::ys -> if playRound (c1,c2) then
+                            printfn "Player 1 hand"
+                            playGame(xs@(c1::c2::[]),ys)
+                       else
+                            printfn "Player 2 hand"
+                            playGame(xs,ys@(c1::c2::[]))
 
 let ranks =
     [   for v in 2 .. 10 -> Value v
@@ -35,7 +55,34 @@ let ranks =
 let deck = seq {
     for suit in suits do
         for rank in ranks -> suit,rank }
-*)
+
+let rand = new System.Random()
+type Hands = Card list*Card list
+ 
+let drop i l =
+    let rec drop' i l1 p l2 =
+        if List.length l2 = List.length l1 - 1 then l2
+        else 
+            let p = if p = i then p+1  else p 
+            drop' i l1 (p+1) (List.append l2 [(List.item p l1)])
+    drop' i l 0 [] 
+
+let getCard d cs =
+    let i = rand.Next(0, List.length d)
+    let h1Cards = d.[i]::cs
+    h1Cards,drop i d
+
+let getHands dec =
+    let rec getHandAccs d (h1,h2) =
+        if List.length d = 0 then (h1,h2)
+        else 
+            let h1Cards, d1 = getCard d h1
+            let h2Cards, d2 = getCard d1 h2
+            getHandAccs d2 (h1Cards,h2Cards)
+    getHandAccs (Seq.toList dec) ([],[])
+
+let h1,h2 = getHands deck           
+playGame (h1,h2)
 
 // fill in tests for your game
 let tests () =
